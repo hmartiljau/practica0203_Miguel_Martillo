3f95dd4d716a21a71af61d9ba50d818b4e1b0471
esto ya no es tuyo <jguimaroro@educacion.navarra.es>
2024-06-10 18:47:27 +0200

    ejercicio10

---
+++ ejercicio10.py
@@ -0,0 +1,5 @@
+numeros = [50, 75, 46, 22, 80, 65, 8]
+numero_mayor = max(numeros)
+numero_menor = min(numeros)
+print("El número mayor es:", numero_mayor)
+print("El número menor es:", numero_menor)
